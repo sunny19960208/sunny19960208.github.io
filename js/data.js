10cedec6ead5e286e2c15fcaf1f9a8698ab6c089
sunny19960208 <124685909+sunny19960208@users.noreply.github.com>
2023-08-01 12:51:27 +0800

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,197 @@
+window.data = {
+    "pools": {
+        "event_20160411": {
+            "name": "常驻池子",
+            "banner": "pool/pool1.png",
+            "enable": 1,
+
+            "servant_ssr_up": [
+               
+            ],
+
+            "servant_sr_up": [],
+
+            "servant_r_up": [],
+
+            "craft_essence_ssr_up": [],
+
+            "craft_essence_sr_up": [],
+
+            "craft_essence_r_up": [],
+			
+            "servant_ssr": [
+			    "1001","1002","1003"
+			],
+				"craft_essence_ssr": [
+                "1004","1005"
+            ],
+
+
+            "servant_sr": [
+				"1006","1007"
+			],
+
+            "craft_essence_sr": [
+                "997"
+            ],
+
+
+            "servant_r": [
+				"997"
+			],
+
+           
+            "craft_essence_r": [
+                "997"
+            ],
+        },
+
+     /*  "event_summer_1": {
+            "name": "夏日推荐召唤1",
+            "banner": "pool/1.jpg",
+            "enable": 1,
+
+             "servant_ssr_up": [],
+
+            "servant_sr_up": [],
+
+            "servant_r_up": [],
+
+            "craft_essence_ssr_up": [],
+
+            "craft_essence_sr_up": [],
+
+            "craft_essence_r_up": [],
+
+            "servant_ssr": [
+                "006"
+				],
+
+            "servant_sr": [
+                "000"
+            ],
+
+            "servant_r": [
+                "099"
+            ],
+
+            "craft_essence_ssr": [
+               "006","014","015","038","032","033"
+            ],
+
+            "craft_essence_sr": [
+                "000"
+            ],
+
+            "craft_essence_r": [
+                "099"
+            ],
+
+	   },
+       "event_summer_2": {
+            "name": "夏日推荐召唤2",
+            "banner": "pool/2.jpg",
+            "enable": 1,
+            "servant_ssr_up": [
+                {
+                    
+                },
+            ],
+
+            "servant_sr_up": [],
+
+            "servant_r_up": [],
+
+            "craft_essence_ssr_up": [],
+
+            "craft_essence_sr_up": [
+                {
+                    "id": "000",
+                    "rate": 0.08
+                },
+                {
+                    "id": "000",
+                    "rate": 0.08
+                },
+                {
+                    "id": "000",
+                    "rate": 0.08
+                },
+            ],
+
+            "craft_essence_r_up": [],
+
+            "servant_ssr": [
+                "006","007","008"
+                ,"014","015","021","023","026","027","029","030","031","032","033","034","035","036","037","038"
+            ],
+
+            "servant_sr": [
+                "000"
+            ],
+
+            "servant_r": [
+                "099"
+            ],
+
+            "craft_essence_ssr": [
+               "006","014","015","038","032","033"
+            ],
+
+            "craft_essence_sr": [
+                "000"
+            ],
+
+            "craft_essence_r": [
+                "099"
+            ],
+        },
+        "event_mock": {
+            "name": "卡池信息模板",
+            "banner": "img/banners/mock.png",
+            "enable": 0,
+            "servant_ssr_up": [
+                {
+                    "id": "000",
+                    "rate": 0.68
+                }
+            ],
+            "servant_sr_up": [
+                {
+                    "id": "000",
+                    "rate": 0.5
+                }
+            ],
+            "servant_r_up": [
+                {
+                    "id": "000",
+                    "rate": 0.25
+                }
+            ],
+            "craft_essence_ssr_up": [
+                {
+                    "id": "000",
+                    "rate": 0.25
+                }
+            ],
+            "craft_essence_sr_up": [
+                {
+                    "id": "000",
+                    "rate": 0.333333
+                }
+            ],
+            "craft_essence_r_up": [
+                {
+                    "id": "000",
+                    "rate": 0.25
+                }
+            ],
+            "servant_ssr": ["000"],
+            "servant_sr": ["000"],
+            "servant_r": ["000"],
+            "craft_essence_ssr": ["000"],
+            "craft_essence_sr": ["000"],
+            "craft_essence_r": ["000"]
+        }*/
+    }
+}
